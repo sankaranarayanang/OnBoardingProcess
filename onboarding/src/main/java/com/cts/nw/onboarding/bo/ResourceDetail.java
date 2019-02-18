b9ebda40de6d443911e878a3f11fa36b3575a254
Suresh <suresh.baskar2@cognizant.com>
2019-02-18 15:05:10 +0530

    Updated CHnages

--- onboarding/src/main/java/com/cts/nw/onboarding/bo/ResourceDetail.java
+++ onboarding/src/main/java/com/cts/nw/onboarding/bo/ResourceDetail.java
@@ -57,7 +57,6 @@ public class ResourceDetail implements Serializable{
 	private String lastName;
 	
 	@Column(name="DOB")
-	@Temporal(TemporalType.DATE)
 	private Date dateOfBirth;
 	
 	@Column(name="PassportNumber")
@@ -82,11 +81,9 @@ public class ResourceDetail implements Serializable{
 	private String location;
 	
 	@Column(name="StartDate")
-	@Temporal(TemporalType.DATE)
 	private Date startDate;
 	
 	@Column(name="EndDate")
-	@Temporal(TemporalType.DATE)
 	private Date endDate;
 	
 	@Column(name="NationwideId")
--- onboarding/src/main/webapp/WEB-INF/views/requestUpdate.jsp
+++ onboarding/src/main/webapp/WEB-INF/views/requestUpdate.jsp
@@ -4,7 +4,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-<meta name="viewport" content="width=device-width, initial-scale=1">.js"></script> -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Onboarding Resources</title>
 </head>
 <body>
