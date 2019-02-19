d003b0919860da47ad0d36c756f00f373fadc53b
suresh <suresh.baskar2@cognizant.com>
2019-02-19 23:42:07 +0530

    Project details

---
+++ onboarding/src/main/java/com/cts/nw/onboarding/bo/ProjectDetail.java
@@ -0,0 +1,92 @@
+/**
+ * 
+ */
+package com.cts.nw.onboarding.bo;
+
+/**
+ * @author 656579
+ *
+ */
+public class ProjectDetail {
+	
+	private String projectName;
+	private Integer projectId;
+	private Integer requesterID;
+	private String requesterName;
+	private Integer approverID;
+	private String approverName;
+	/**
+	 * @return the projectName
+	 */
+	public String getProjectName() {
+		return projectName;
+	}
+	/**
+	 * @param projectName the projectName to set
+	 */
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+	/**
+	 * @return the projectId
+	 */
+	public Integer getProjectId() {
+		return projectId;
+	}
+	/**
+	 * @param projectId the projectId to set
+	 */
+	public void setProjectId(Integer projectId) {
+		this.projectId = projectId;
+	}
+	/**
+	 * @return the requesterID
+	 */
+	public Integer getRequesterID() {
+		return requesterID;
+	}
+	/**
+	 * @param requesterID the requesterID to set
+	 */
+	public void setRequesterID(Integer requesterID) {
+		this.requesterID = requesterID;
+	}
+	/**
+	 * @return the requesterName
+	 */
+	public String getRequesterName() {
+		return requesterName;
+	}
+	/**
+	 * @param requesterName the requesterName to set
+	 */
+	public void setRequesterName(String requesterName) {
+		this.requesterName = requesterName;
+	}
+	/**
+	 * @return the approverID
+	 */
+	public Integer getApproverID() {
+		return approverID;
+	}
+	/**
+	 * @param approverID the approverID to set
+	 */
+	public void setApproverID(Integer approverID) {
+		this.approverID = approverID;
+	}
+	/**
+	 * @return the approverName
+	 */
+	public String getApproverName() {
+		return approverName;
+	}
+	/**
+	 * @param approverName the approverName to set
+	 */
+	public void setApproverName(String approverName) {
+		this.approverName = approverName;
+	}
+	
+
+}
